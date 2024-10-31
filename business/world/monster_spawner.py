c6505cc0f75a6d86d3dbadc910e88416fc32651a
Santino Andreatta <149499721+CuackXI@users.noreply.github.com>
2024-10-31 16:22:00 -0300

    ARREGLAR CIRCULAR IMPORTS DEL RUNNER

--- business/world/monster_spawner.py
+++ business/world/monster_spawner.py
@@ -11,7 +11,6 @@ from business.world.interfaces import IGameWorld, IMonsterSpawner
 from presentation.sprite import MonsterSprite
 import business.handlers.cooldown_handler as CH
 from business.exceptions import EntityOutOfBounds
-from runner import display
 
 class MonsterSpawner(IMonsterSpawner):
     """Spawns monsters in the game world."""
@@ -21,45 +20,44 @@ class MonsterSpawner(IMonsterSpawner):
     def __init__(self):
         self.__spawn_cooldown = CH.CooldownHandler(MonsterSpawner.BASE_DELAY)
         self.__logger = logging.getLogger(__name__)
-        self.__display = display
 
     def update(self, world: IGameWorld):
         if self.__spawn_cooldown.is_action_ready() and len(world.monsters) <= 20:
             self.spawn_monster(world)
             self.__spawn_cooldown.put_on_cooldown()
 
-    def spawn_monster(self, world: IGameWorld, camera):
-        """Spawns a monster at the edge of the current camera view.
-
-        Args:
-            world (IGameWorld): The game world instance.
-            camera (Camera): The camera instance.
-        """
+    def spawn_monster(self, world: IGameWorld):
+        #TODO: CAMBIAR ESTO PARA QUE USE LOS BORDES DE LA CAMARA (TIRA CIRCULAR IMPORT CUALQUIER IMPORT DEL RUNNER)
         while True:
             try:
-                camera_left = self.__display.camera.camera_rect.left
-                camera_right = self.__display.camera.camera_rect.right
-                camera_top = self.__display.camera.camera_rect.top
-                camera_bottom = self.__display.camera.camera_rect.bottom
+                player_x = int(world.player.pos_x)
+                player_y = int(world.player.pos_y)
+                screen_width = settings.SCREEN_WIDTH // 2
+                screen_height = settings.SCREEN_HEIGHT // 2
+
+                left_edge = player_x - screen_width
+                right_edge = player_x + screen_width
+                top_edge = player_y - screen_height
+                bottom_edge = player_y + screen_height
 
                 edge = random.choice(['top', 'bottom', 'left', 'right'])
 
                 if edge == 'top':
-                    pos_x = random.randint(camera_left, camera_right)
-                    pos_y = camera_top
+                    pos_x = random.randint(left_edge, right_edge)
+                    pos_y = top_edge
                 elif edge == 'bottom':
-                    pos_x = random.randint(camera_left, camera_right)
-                    pos_y = camera_bottom
+                    pos_x = random.randint(left_edge, right_edge)
+                    pos_y = bottom_edge
                 elif edge == 'left':
-                    pos_x = camera_left
-                    pos_y = random.randint(camera_top, camera_bottom)
+                    pos_x = left_edge
+                    pos_y = random.randint(top_edge, bottom_edge)
                 else:
-                    pos_x = camera_right
-                    pos_y = random.randint(camera_top, camera_bottom)
+                    pos_x = right_edge
+                    pos_y = random.randint(top_edge, bottom_edge)
 
                 monster = Monster(pos_x, pos_y, MonsterSprite(pos_x, pos_y))
                 world.add_monster(monster)
 
-                break 
+                break
             except EntityOutOfBounds:
-                print("Trying another position for spawning monster.")
+                print("Spawneando en otra posicion :v")
